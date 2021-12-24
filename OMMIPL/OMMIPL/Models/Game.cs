4f1cb8a896d4b9c42c8f5c8666ad570c366d5379
unknown <afluexdevelopment@gmail.com>
2021-12-24 19:00:51 +0530

    Changes

---
+++ OMMIPL/OMMIPL/Models/Game.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace OMMIPL.Models
+{
+    public class Game
+    {
+        public string FK_GameId { get; set; }
+        public string FK_ColorId { get; set; }
+        public string FK_NumberId { get; set; }
+        public decimal Amount { get; set; }
+        public string Duration { get; set; }
+        public string GameName { get; set; }
+        public string ColorName { get; set; }
+        public List<Game> lst { get; set; }
+        public string Document { get; set; }
+        public string Image { get; set; }
+        public DataSet GetAllGames()
+        {
+            DataSet ds = DBHelper.ExecuteQuery("GetGameDetails");
+            return ds;
+        }
+        public DataSet GetGameDetailsById()
+        {
+            SqlParameter[] para = { new SqlParameter ("@GameId", FK_GameId )};
+            DataSet ds = DBHelper.ExecuteQuery("GetGameById",para);
+            return ds;
+        }
+    }
+}
