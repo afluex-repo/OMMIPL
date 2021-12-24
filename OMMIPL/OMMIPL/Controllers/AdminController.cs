3168ee3b3400cbafeed477e45a39cba1179770e3
unknown <afluexdevelopment@gmail.com>
2021-12-24 11:32:44 +0530

    Changes

---
+++ OMMIPL/OMMIPL/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OMMIPL.Controllers
+{
+    public class AdminController : BaseController
+    {
+        // GET: User
+        public ActionResult AdminDashboard()
+        {
+            return View();
+        }
+    }
+}
